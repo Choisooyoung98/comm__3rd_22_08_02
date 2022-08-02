b7278115d13c1657c7da42ea1e7ad34bcfecee8f
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-02 13:24:00 +0900

    new 없이 객체 생성, Ut.cls.newObj로 객체 생성 제네릭 적용하여 형변환 팔요성 제거, slf4j-nop추가

--- src/main/java/com/ll/exam/Con.java
+++ src/main/java/com/ll/exam/Con.java
@@ -2,34 +2,34 @@ package com.ll.exam;
 
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.article.controller.ArticleController;
+import com.ll.exam.home.controller.HomeController;
 import org.reflections.Reflections;
 
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.ll.exam.Ut.str.decapitalize;
-
 public class Con {
     private static final ArticleController articleController;
+    private static final HomeController homeController;
     static {
-        articleController = new ArticleController();
+        articleController = (ArticleController) Ut.cls.newObj(ArticleController.class, null);
+        homeController = (HomeController) Ut.cls.newObj(HomeController.class, null);
     }
     public static ArticleController getArticleController() {
         return articleController;
     }
+
 
     public static List<String> getControllerNames() {
         List<String> names = new ArrayList<>();
 
         Reflections reflections = new Reflections("com.ll.exam");
         for (Class<?> cls : reflections.getTypesAnnotatedWith(Controller.class)) {
-            String name = cls.getSimpleName();
-            name = name.replace("Controller","");
+            String name = cls.getSimpleName().replace("Controller","");
 
-            name = decapitalize(name);
-
-            names.add(name);
+            names.add(Ut.str.decapitalize(name));
         }
 
         return names;
